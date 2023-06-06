6c17867c6903b1059eadbaf7f2baa899effe5bf0
Mio-FCIP <1099834705@qq.com>
2023-06-06 13:57:46 +0800

    modified:   README.md 	modified:   app.js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const fs = require('fs');
+import express, { json } from 'express';
+import { readFileSync } from 'fs';
 const app = express();
 
+app.use(json()); // 为解析 POST 请求中的 JSON 添加中间件
+
 // 读取 data.json 文件中的数据
-const data = JSON.parse(fs.readFileSync('data.json'));
+const data = JSON.parse(readFileSync('data.json'));
 
 // 处理 GET 请求
 app.get('/api', (req, res) => {
-  // 随机选择一条文本
-  const randomIndex = Math.floor(Math.random() * data.messages.length);
-  // 将文本中的 FCIP 字符串替换为请求中的参数或者默认值
-  const text = data.messages[randomIndex].replace(/FCIP/g, req.query.name || 'FCIP');
-  // 返回处理后的结果
-  res.send({ text });
+  handleRequest(req, res);
 });
 
 // 处理 POST 请求
 app.post('/api', (req, res) => {
-  // 读取 data.json 文件中的数据
-  const data = JSON.parse(fs.readFileSync('data.json'));
+  handleRequest(req, res);
+});
+
+function handleRequest(req, res) {
   // 随机选择一条文本
   const randomIndex = Math.floor(Math.random() * data.messages.length);
+  // 将文本中的 FCIP 字符串替换为请求中的参数或者默认值
+  const text = data.messages[randomIndex].replace(/FCIP/g, req.query.name || req.body.name || 'FCIP');
   // 返回处理后的结果
-  const text = data.messages[randomIndex];
   res.send({ text });
-});
+}
 
 // 启动服务器
 app.listen(3000, () => {
