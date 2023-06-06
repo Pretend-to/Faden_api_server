f78dace2c5c3f74a419b527f6a218d47ab7f8446
Mio-FCIP <1099834705@qq.com>
2023-06-06 20:05:39 +0800

    modified:   app.js 	modified:   data.json

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express, { json } from 'express';
 import { readFileSync } from 'fs';
+import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
+
 const app = express();
 
 app.use(json()); // 为解析 POST 请求中的 JSON 添加中间件
@@ -7,13 +10,23 @@ app.use(json()); // 为解析 POST 请求中的 JSON 添加中间件
 // 读取 data.json 文件中的数据
 const data = JSON.parse(readFileSync('data.json'));
 
+// 显示日志
+app.use(morgan('tiny'));
+
+// 添加基于 IP 地址的速率限制
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 限制的时间窗口，这里是 15 分钟
+  max: 100, // 在窗口期内允许的最大请求次数
+  message: '请求过于频繁，请稍后再试。' // 超过请求次数限制时返回的错误消息
+});
+
 // 处理 GET 请求
-app.get('/api', (req, res) => {
+app.get('/api', limiter, (req, res) => {
   handleRequest(req, res);
 });
 
 // 处理 POST 请求
-app.post('/api', (req, res) => {
+app.post('/api', limiter, (req, res) => {
   handleRequest(req, res);
 });
 
@@ -22,6 +35,8 @@ function handleRequest(req, res) {
   const randomIndex = Math.floor(Math.random() * data.messages.length);
   // 将文本中的 FCIP 字符串替换为请求中的参数或者默认值
   const text = data.messages[randomIndex].replace(/FCIP/g, req.query.name || req.body.name || 'FCIP');
+  // 记录请求日志
+  console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url} - ${res.statusCode}`);
   // 返回处理后的结果
   res.send({ text });
 }
