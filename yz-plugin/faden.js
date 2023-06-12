536a449a4b38c3f9b0699f053afea017c9731429
Mio-FCIP <1099834705@qq.com>
2023-06-12 12:46:42 +0800

    new file:   yz-plugin/cn_poken.js 	new file:   yz-plugin/faden.js 	new file:   yz-plugin/poken.js

---
+++ yz-plugin/faden.js
@@ -0,0 +1,51 @@
+import plugin from '../../lib/plugins/plugin.js';
+import { segment } from 'icqq';
+
+export class Mio extends plugin {
+  constructor(e) {
+    super({
+      name: '澪',
+      dsc: '自己做着玩的',
+      event: 'message',
+      priority: 1000,
+      rule: [{
+        reg: ".*[！!]$",
+        fnc: 'show'
+      }]
+    });
+  }
+
+  async show(e) {
+    console.log(e);
+    let name;
+    let raw = e.msg.replace(/^[！!]+|[！!]+$/g, "");
+    console.log(raw);
+    if (raw.length > 3 ) {
+      return;
+    }  
+    if (e.message.filter(m => m.type === 'at').length === 1) {
+      name = e.message.find(m => m.type === 'at').text.replace(/@/g, '');
+    } else if (raw) {
+      name = raw;
+    } else {
+      name = e.sender.title ? e.sender.title : e.sender.card;
+    }
+    let url = `https://api.fcip.xyz/faden/api?name=${name}`;
+    try {
+      let response = await fetch(url);
+      if (response.status === 200) {
+        let json = await response.json();
+        if (json.text) {
+          await this.reply(json.text, false);
+        } else {
+          await e.reply('连接api接口失败！错误原因：' + json.toString());
+          return true;
+        }
+      }
+    } catch (err) {
+      logger.error('连接api接口失败！错误原因：', err);
+      await e.reply('连接api接口失败！错误原因：' + err);
+      return false;
+    }
+  }
+}
