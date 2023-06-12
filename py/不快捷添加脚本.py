cd5635a51a904949605897420f10b956a2d18bce
Mio-FCIP <1099834705@qq.com>
2023-06-12 12:20:05 +0800

    modified:   data.json 	modified:   py/data.json 	modified:   "py/\344\270\215\345\277\253\346\215\267\346\267\273\345\212\240\350\204\232\346\234\254.py"

--- py/不快捷添加脚本.py
+++ py/不快捷添加脚本.py
@@ -1,8 +1,12 @@
+import os
 import json
 import re
 
-# 读取 data.json 文件
-with open('data.json', 'r', encoding='utf-8') as f:
+# 获取当前脚本文件所在的目录
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# 读取父文件夹中的 data.json 文件
+with open(os.path.join(script_dir, '..', 'data.json'), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 # 打印当前 data.json 中的 messages 列表
@@ -22,8 +26,8 @@ while True:
         message = re.sub(keyword, 'FCIP', message)
         # 将新的 message 添加到 messages 列表中
         data['messages'].append(message)
-        # 将更新后的 data.json 保存到文件中
-        with open('data.json', 'w', encoding='utf-8') as f:
+        # 将更新后的 data.json 保存到父文件夹中
+        with open(os.path.join(script_dir, '..', 'data.json'), 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         print('添加成功！')
     else:
@@ -33,4 +37,3 @@ while True:
     choice = input('是否继续添加？（Y/N）（直接回车表示继续添加）')
     if choice.lower() == 'n':
         break
-
